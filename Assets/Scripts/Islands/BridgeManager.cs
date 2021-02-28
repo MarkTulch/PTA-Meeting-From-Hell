56a45f972d64b155aeed867a3359a0f926e92062
asbutt@ualberta.ca <pakasfand@gmail.com>
2021-02-27 21:45:05 -0800

    Add in basic pathfinding

--- Assets/Scripts/Islands/BridgeManager.cs
+++ Assets/Scripts/Islands/BridgeManager.cs
@@ -3,31 +3,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class BridgeDecision
+{
+    [SerializeField] public List<BridgeController> option1;
+    [SerializeField] public List<BridgeController> option2;
+}
+
 public class BridgeManager : MonoBehaviour
 {
-    private List<BridgeController> _bridges;
-    private void OnEnable()
-    {
-        BridgeController.OnBridgeCreated += OnBridgeCreated;
-    }
+    //private List<BridgeController> _bridges;
+    //private void OnEnable()
+    //{
+    //    BridgeController.OnBridgeCreated += OnBridgeCreated;
+    //}
 
-    private void OnDisable()
-    {
-        BridgeController.OnBridgeCreated -= OnBridgeCreated;
-    }
+    //private void OnDisable()
+    //{
+    //    BridgeController.OnBridgeCreated -= OnBridgeCreated;
+    //}
 
-    private void OnBridgeCreated(BridgeController bridgeController)
+    //private void OnBridgeCreated(BridgeController bridgeController)
+    //{
+    //    foreach (var bridge in _bridges)
+    //    {
+    //        if(bridge == bridgeController) { continue; }
+
+    //        bridge.DisableBridge();
+    //    }
+    //}
+
+    //private void Awake()
+    //{
+    //    _bridges = new List<BridgeController>(FindObjectsOfType<BridgeController>());
+    //}
+
+    [SerializeField] private List<BridgeDecision> _bridgeDecisions;
+
+    private void Start()
     {
-        foreach (var bridge in _bridges)
+        DisableAllDecisionBridges();
+
+        foreach (var decision in _bridgeDecisions)
         {
-            if(bridge == bridgeController) { continue; }
+            var rand = UnityEngine.Random.Range(0.0f, 1.0f);
 
-            bridge.DisableBridge();
+            if (rand >= 0.5f)
+            {
+                foreach (var bridge in decision.option1)
+                {
+                    bridge.EnableBridge();
+                }
+            }
+            else
+            {
+                foreach (var bridge in decision.option2)
+                {
+                    bridge.EnableBridge();
+                }
+            }
         }
+        AstarPath.active.Scan();
     }
 
-    private void Awake()
+    private void DisableAllDecisionBridges()
     {
-        _bridges = new List<BridgeController>(FindObjectsOfType<BridgeController>());
+        foreach (var decision in _bridgeDecisions)
+        {
+            foreach (var bridge in decision.option1)
+            {
+                bridge.DisableBridge();
+            }
+            foreach (var bridge in decision.option2)
+            {
+                bridge.DisableBridge();
+            }
+        }
     }
 }
