220116f5a1ff50dcfb8f534115662e6eaff569ce
asbutt@ualberta.ca <pakasfand@gmail.com>
2021-02-28 00:48:39 -0800

    Add waveManager and GameStateManager

--- Assets/Scripts/Islands/BridgeManager.cs
+++ Assets/Scripts/Islands/BridgeManager.cs
@@ -12,35 +12,14 @@ public class BridgeDecision
 
 public class BridgeManager : MonoBehaviour
 {
-    //private List<BridgeController> _bridges;
-    //private void OnEnable()
-    //{
-    //    BridgeController.OnBridgeCreated += OnBridgeCreated;
-    //}
-
-    //private void OnDisable()
-    //{
-    //    BridgeController.OnBridgeCreated -= OnBridgeCreated;
-    //}
-
-    //private void OnBridgeCreated(BridgeController bridgeController)
-    //{
-    //    foreach (var bridge in _bridges)
-    //    {
-    //        if(bridge == bridgeController) { continue; }
-
-    //        bridge.DisableBridge();
-    //    }
-    //}
-
-    //private void Awake()
-    //{
-    //    _bridges = new List<BridgeController>(FindObjectsOfType<BridgeController>());
-    //}
-
     [SerializeField] private List<BridgeDecision> _bridgeDecisions;
 
     private void Start()
+    {
+        ShuffleBridges();
+    }
+
+    public void ShuffleBridges()
     {
         DisableAllDecisionBridges();
 
---
+++ Assets/Scripts/Islands/GameStateManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _complaintsText;
+    [SerializeField] private Image _stopwatchImage;
+
+    [SerializeField] private float _roundTimeInMins;
+
+    private float _gameTimer;
+    private int _complaints = 0;
+
+    public static Action OnPlayerWins;
+    public static Action OnPlayerLoses;
+
+    private void OnEnable()
+    {
+        EnemyTracker.OnEnemyReachedDestination += IncrementComplaints;
+
+        _complaintsText.text = $"Complaints {_complaints}/10";
+    }
+
+    private void OnDisable()
+    {
+        EnemyTracker.OnEnemyReachedDestination += IncrementComplaints;
+    }
+
+    private void IncrementComplaints()
+    {
+        _complaints += 1;
+
+        _complaintsText.text = $"Complaints {_complaints}/10";
+    }
+
+    void Update()
+    {
+        _gameTimer += Time.deltaTime;
+        _stopwatchImage.fillAmount = _gameTimer / (_roundTimeInMins * 60);
+
+        CheckIfPlayerWins();
+
+        CheckIfPlayerLost();
+    }
+
+    private void CheckIfPlayerWins()
+    {
+        if (_gameTimer >= _roundTimeInMins)
+        {
+            OnPlayerWins?.Invoke();
+        }
+    }
+
+    private void CheckIfPlayerLost()
+    {
+        if (_complaints > 10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            OnPlayerLoses?.Invoke();
+        }
+    }
+}
